ab9c57a4d1190562822fff116a4cf6db4a377feb
Sean Cooper <seancooper@usf.edu>
2025-10-03 00:09:27 -0400

    initial commit - step 2 done

---
+++ cc_6.js
@@ -0,0 +1,19 @@
+// Coding Challenge 06
+// Step 1 - Complete 
+
+// Step 2
+class Employee {
+    constructor(name, department) {
+        this.name = name; 
+        this.department = department;
+        
+    }
+
+
+describe() {
+    return `${this.name} currently works in the ${this.department} department.`
+
+    }
+}
+
+// Code goes here
